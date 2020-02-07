decc7caade57066ef41efd1f8a5294f9ec86a3da
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-02-07 15:38:29 +0100

    refactor property benchmark to associative array.

--- test/propertyBenchmark.js
+++ test/propertyBenchmark.js
@@ -2,50 +2,48 @@
 import * as rdf from '../js/rdf.js';
 
 const propertyBenchmark =
-[
+{
+  "http://hitontology.eu/ontology/FeatureClassified":
   {
-    class: "hito:FeatureClassified",
     graph: "http://hitontology.eu/ontology",
     endpoint: "https://hitontology.eu/sparql",
     instances: ["hito:AccessByClientToOwnMedicalRecords"],
   },
+  "http://hitontology.eu/ontology/EnterpriseFunctionClassified":
   {
-    class: "hito:EnterpriseFunctionClassified",
     graph: "http://hitontology.eu/ontology",
     endpoint: "https://hitontology.eu/sparql",
     instances: ["hito:FacilityManagement"],
   },
-  {
-    class: "hito:UserGroupClassified",
-    graph: "http://hitontology.eu/ontology",
-    endpoint: "https://hitontology.eu/sparql",
-    instances: ["hito:Pharmacist_occupation"],
-  },
-  {
-    class: "http://dbpedia.org/class/yago/OperatingSystem106568134",
-    graph: "http://dbpedia.org",
-    endpoint: "https://dbpedia.org/sparql",
-    instances: ["http://dbpedia.org/resource/Windows_95"],
-  },
-  {
-    class: "hito:OrganizationalUnitClassified",
-    graph: "http://hitontology.eu/ontology",
-    endpoint: "https://hitontology.eu/sparql",
-    instances: ["hito:Care_of_the_elderly_department_environment"],
-  },
-  {
-    class: "hito:DatabaseSystem",
-    graph: "http://hitontology.eu/ontology",
-    endpoint: "https://hitontology.eu/sparql",
-    instances: ["hito:MySql"],
-  },
-
-];
-
-for(const entry of propertyBenchmark)
+  "http://hitontology.eu/ontology/UserGroupClassified":
 {
-  entry.class = rdf.long(entry.class);
-  for(const x in entry.instances) {entry.instances[x]=rdf.long(entry.instances[x]);}
+  graph: "http://hitontology.eu/ontology",
+  endpoint: "https://hitontology.eu/sparql",
+  instances: ["hito:Pharmacist_occupation"],
+},
+  "http://dbpedia.org/class/yago/OperatingSystem106568134":
+{
+  graph: "http://dbpedia.org",
+  endpoint: "https://dbpedia.org/sparql",
+  instances: ["dbr:Windows_95"],
+},
+  "http://hitontology.eu/ontology/OrganizationalUnitClassified":
+{
+  graph: "http://hitontology.eu/ontology",
+  endpoint: "https://hitontology.eu/sparql",
+  instances: ["hito:Care_of_the_elderly_department_environment"],
+},
+  "http://hitontology.eu/ontology/DatabaseSystem":
+{
+  graph: "http://hitontology.eu/ontology",
+  endpoint: "https://hitontology.eu/sparql",
+  instances: ["hito:MySql"],
+},
+};
+
+for(const x in propertyBenchmark)
+{
+  propertyBenchmark[x].instances=propertyBenchmark[x].instances.map(i=>rdf.long(i));
 }
 
 export default propertyBenchmark;
