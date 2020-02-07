aeda55e43cd8f757ee802ed38d27c8f574f1512e
ThomasPause <tp55sude@studserv.uni-leipzig.de>
2020-02-07 12:01:31 +0100

    Uopdate benchamrk with test instances, see #9

--- test/benchmark.js
+++ test/benchmark.js
@@ -10,10 +10,34 @@ const benchmark =
     instance: "hito:AccessByClientToOwnMedicalRecords",
     queries: ["Access by client to own medical records", "Access by client", "Aces by client to medical record"],
   },
-/*["hito:FunctionClassified",""],
-["hito:UserGroupClassified",""],
-["hito:EnterpriseFunctionClassified",""],
-["hito:OrganizationalUnitClassified",""]]*/
+  {
+    class: "hito:EnterpriseFunctionClassified",
+    graph: "http://hitontology.eu/ontology",
+    endpoint: "https://hitontology.eu/sparql",
+    instance: "hito:FacilityManagement",
+    queries: ["Facility Management", "FacilityManagement", "facility management", "facility", "management", "Facilty Managemen", "facilityMangement"],
+  },
+  {
+    class: "hito:UserGroupClassified",
+    graph: "http://hitontology.eu/ontology",
+    endpoint: "https://hitontology.eu/sparql",
+    instance: "hito:Pharmacist_occupation",
+    queries: ["Pharmacist occupation", "Pharmacist", "occupation", "Pharmcist ocupation"],
+  },
+  {
+    class: "http://dbpedia.org/class/yago/OperatingSystem106568134",
+    graph: "http://dbpedia.org/",
+    endpoint: "http://dbpedia.org/sparql",
+    instance: "http://dbpedia.org/resource/Windows_95",
+    queries: ["Windows 95", "Windows", "Win", "Win 95", "WIN95", "Wimdows 95"],
+  },
+  {
+    class: "hito:OrganizationalUnitClassified",
+    graph: "http://hitontology.eu/ontology",
+    endpoint: "https://hitontology.eu/sparql",
+    instance: "hito:Care_of_the_elderly_department_environment",
+    queries: ["Care_of_the_elderly_department_environment", "Care of the elderly department environment", "Care of the elderly", "department environment", "elderly department", "care the elderly department"],
+  },
 ];
 
 for(const entry of benchmark)
