6f7fcd509ff3bd456ac4eb21373c4323e897cd28
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-02-14 11:41:27 +0100

    Add used yago classes manually.

--- js/clazz.js
+++ js/clazz.js
@@ -8,7 +8,7 @@ const classes = new Map();
 
 class Clazz extends Resource
 {
-  /** */
+  /** Treats the resource as a class*/
   constructor(resource)
   {
     super(resource.uri,resource.labels,resource.altLabels,resource.comments);
@@ -26,12 +26,14 @@ class Clazz extends Resource
     if(this.uri.includes("dbpedia.org/")) {sources = [sparql.DBPEDIA];}
     else if(this.uri.includes("hitontology.eu/"))  {sources = [sparql.HITO];}
     else {sources = [sparql.HITO,sparql.DBPEDIA];}
+    const pattern= `?uri a <${this.uri}>.`;
+    //if(this.uri===rdf.long("owl:Class")) {pattern = "{?uri a owl:Class} UNION {?uri owl:equivalentClass ?x}";} // DBpedia yago workaround
     const query  = `SELECT ?uri
     GROUP_CONCAT(DISTINCT(CONCAT(?l,"@",lang(?l)));SEPARATOR="|") AS ?l
     GROUP_CONCAT(DISTINCT(CONCAT(?al,"@",lang(?al)));SEPARATOR="|") AS ?al
     GROUP_CONCAT(DISTINCT(CONCAT(?cmt,"@",lang(?cmt)));SEPARATOR="|") AS ?cmt
     {
-      ?uri a <${this.uri}>.
+      ${pattern}
       OPTIONAL {?uri rdfs:label ?l.}
       OPTIONAL {?uri skos:altLabel ?al.}
       OPTIONAL {?uri skos:altLabel ?cmt.}
@@ -41,12 +43,10 @@ class Clazz extends Resource
     {
       bindings.push(...sparql.flat(await sparql.select(query,source,`select all instances of class ${this.uri} from `+source.name)));
     }
-    //console.log(bindings);
     this.instances = [];
     const unpack = s => (s && (s!=="@") && s.split('|')) || []; // "@" occurs on 0 results
     bindings.forEach(b=>
     {
-      if(b.uri.includes("dbpedia")) {console.log("***************************************************************",b);}
       {
         this.instances.push(
           new Resource(b.uri,unpack(b.l),unpack(b.al),unpack(b.cmt)));
@@ -60,6 +60,15 @@ class Clazz extends Resource
 /** @type {Promise<Class>}  */
 let owlClassInstances = null;
 
+// define any other classes here
+// include yago classes manually as there are too many of them in DBpedia to fetch them all and they don't have type owl:Class
+
+const customClassData = [["yago:ProgrammingLanguage106898352","Programming Language"],
+  ["yago:License106549661","License"],
+  ["yago:OperatingSystem106568134","Operating System"]];
+const customClassInstances = customClassData.map(([uri,label]) => new Resource(rdf.long(uri),[label],[],[]));
+
+
 /** Query the class that has the given URI with all its instances.*/
 async function queryClass(uri)
 {
@@ -70,7 +79,7 @@ async function queryClass(uri)
     owlClassInstances = c.loadInstances().then(()=>
     {
       const m = new Map();
-      c.instances.forEach(i=>{m.set(i.uri,i);});
+      [...c.instances,...customClassInstances].forEach(i=>{m.set(i.uri,i);});
       return m;
     },
     );
@@ -86,9 +95,7 @@ async function queryClass(uri)
   Asynchronous multiton pattern, see https://stackoverflow.com/questions/60152736/asynchronous-multiton-pattern-in-javascript.*/
 export default async function getClass(uri)
 {
-  //console.log(uri);
   let clazz = classes.get(uri);
-  //console.log("class",clazz);
   if(!clazz)
   {
     // already called with the same URI, not necessarily finished but we never want to run it twice
