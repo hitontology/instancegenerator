ac439132d87b882a3c79c8f65d5e9175b84e7324
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2020-02-16 13:14:58 +0100

    Further into the rework.

--- js/clazz.js
+++ js/clazz.js
@@ -1,29 +1,25 @@
 /** An OWL class. */
-import {Resource} from "./resource.js";
-//import ResourceIndex from "./resourceIndex.js";
-import * as sparql from "./sparql.js";
 import * as rdf from "./rdf.js";
+import {Resource} from "./resource.js";
 
-const classes = new Map();
-
+/*
 export class Clazz extends Resource
 {
-  /** @param resource Treats the resource as a class
-   * @param relation optional type relation. needs to be enquoted in <> not prefixed or "a".*/
   constructor(uri,labels,altLabels,comments)
   {
     super(uri,labels,altLabels,comments,"a");
   }
 }
+*/
 
 /** @type {Promise<Class>}  */
-const owlClassInstances = null;
+const owlClass = new Resource("http://www.w3.org/2002/07/owl#Class","http://www.w3.org/2002/07/owl#Class",["class"],[],[]);
 
 // define any other classes here
 // include yago classes manually as there are too many of them in DBpedia to fetch them all and they don't have type owl:Class
 
-const customClassData = [["yago:ProgrammingLanguage106898352","Programming Language"],
+const customClasses = [["yago:ProgrammingLanguage106898352","Programming Language"],
   ["yago:License106549661","License"],
   ["yago:OperatingSystem106568134","Operating System"],
-  ["rdfs:Resource","URL"]];
-const customClassInstances = customClassData.map(([uri,label]) => new Resource(rdf.long(uri),[label+"@en"],[],[]));
+  ["rdfs:Resource","URL"]]
+  .map(([uri,label]) => new Resource(rdf.long(uri),[label+"@en"],[],[]));
--- js/resource.js
+++ js/resource.js
@@ -9,12 +9,34 @@ new Map([
   ["http://dbpedia.org/class/yago/License106549661","[] dbo:license ?uri."],
 ]);
 
+const memberRelations =
+new Map([
+  ["owl:Class","rdf:type"],
+  ["hito:FeatureCatalogue","hito:featureCatalogue"],
+  ["hito:FeatureClassified","hito:featureClassified"],
+  //["hito:FeatureCitation","hito:featureC"],
+].map(([x,y])=>[rdf.long(x),rdf.long(y)]));
+
+const resources = new Map();
+
+/** Guess the SPARQL sources of a URI */
+function guessSources(uri)
+{
+  let sources = [];
+  if(this.uri.includes("dbpedia.org/")) {sources = [sparql.DBPEDIA];}
+  else if(this.uri.includes("hitontology.eu/"))  {sources = [sparql.HITO];}
+  else {sources = [sparql.HITO,sparql.DBPEDIA];}
+  return sources;
+}
+
 export class Resource
 {
   /** Labels, altLabels and comments are arrays of strings that can be empty.  */
-  constructor(uri,labels,altLabels,comments)
+  constructor(uri,type,labels,altLabels,comments)
   {
     this.uri = uri;
+    this.type = type;
+    this.memberRelation = memberRelations.get(type);
     this.suffix = rdf.niceSuffix(uri);
     this.labels = labels;
     this.altLabels = altLabels;
@@ -44,18 +66,10 @@ export class Resource
     return this.suffix;
   }
 
-  /** Gets an array containing all members of the given resource using the given membership relation
-  * @param memberRelation membership relation */
-  async loadMembers(memberRelation)
+  /** Gets an array containing all members of the given resource */
+  async queryMembers()
   {
-    if(this.members) {return;} // only done once
-    this.members = [];
-
-    let sources = [];
-    if(this.uri.includes("dbpedia.org/")) {sources = [sparql.DBPEDIA];}
-    else if(this.uri.includes("hitontology.eu/"))  {sources = [sparql.HITO];}
-    else {sources = [sparql.HITO,sparql.DBPEDIA];}
-    let pattern= `?uri ${memberRelation} <${this.uri}>.`;
+    let pattern= `?uri ${this.memberRelation} <${this.uri}>.`;
     const restriction = memberRestrictions.get(this.uri);
     if(restriction) {pattern+="\n"+restriction;}
 
@@ -69,43 +83,53 @@ export class Resource
       OPTIONAL {?uri skos:altLabel ?al.}
       OPTIONAL {?uri skos:altLabel ?cmt.}
     }`;
+
     const bindings = [];
-    for(const source of sources)
+    for(const source of guessSources(this.uri))
     {
       bindings.push(...sparql.flat(await sparql.select(query,source,`select all members of ${this.uri} from `+source.name)));
     }
-    this.members = [];
+    const members = [];
     const unpack = s => (s && (s!=="@") && s.split('|')) || []; // "@" occurs on 0 results
     bindings.forEach(b=>
     {
       {
-        this.members.push(
+        members.push(
           new Resource(b.uri,unpack(b.l),unpack(b.al),unpack(b.cmt)));
       }
     });
     //this.instanceIndex = new ResourceIndex(this.members);
+    return members;
+  }
+
+  /** Cached member function. */
+  async members()
+  {
+    if(!this.members) {this.members=this.queryMembers();} // promise
+    return this.members;
   }
 }
 
 /** Query the resource that has the given URI with all its members.
     @param relation optional type relation*/
-/*
-    async function queryResource(uri)
-    {
-
-    if(!owlClassInstances)
-    {
+async function queryResource(uri)
+{
+  /*
+  if(!owlClassInstances)
+  {
     // initial fetching of all classes
-    const c = new Clazz(new Resource(rdf.long("owl:Class"),[],[],[]),relation);
+    const c = (new Resource(rdf.long("owl:Class"),[],[],[]),relation);
     owlClassInstances = c.loadInstances().then(()=>
     {
-    const m = new Map();
-    [...c.instances,...customClassInstances].forEach(i=>{m.set(i.uri,i);});
-    return m;
-  },
-);
+      const m = new Map();
+      [...c.instances,...customClassInstances].forEach(i=>{m.set(i.uri,i);});
+      return m;
+    },
+    );
+  }
+  */
 }
-*/
+
 /*const instance = (await owlClassInstances).get(uri);
 if(!instance)
 {
@@ -120,17 +144,20 @@ return resource;
 /** Get the resource that has the given URI with all its members. Only one resource is generated for any one URI.
 Asynchronous multiton pattern, see https://stackoverflow.com/questions/60152736/asynchronous-multiton-pattern-in-javascript.
 */
-/*
-export default async function getResource(uri,memberRelation)
+async function getResource(uri)
 {
-let clazz = classes.get(uri);
-if(!clazz)
+  let resource = resources.get(uri);
+  if(!resource)
+  {
+    // already called with the same URI, not necessarily finished but we never want to run it twice
+    // return value is a promise but because the method is async it should be used with await and then it will get unpacked
+    resource = queryResource(uri);
+    resources.set(uri,resource);
+  }
+  return resource;
+}
+
+async function initCache()
 {
-// already called with the same URI, not necessarily finished but we never want to run it twice
-// return value is a promise but because the method is async it should be used with await and then it will get unpacked
-clazz = queryClass(uri,relation);
-classes.set(uri,clazz);
+
 }
-return clazz;
-}
-*/
